b5a723ced0c1281f16d42ecd5bc7e8c6e890a098
sarrufat <sarrufat.feina@gmail.com>
2017-02-19 14:57:28 +0100

    First search menu

--- scripts/app-bundle.js
+++ scripts/app-bundle.js
@@ -2,8 +2,16 @@ define('app',["require", "exports"], function (require, exports) {
     "use strict";
     var App = (function () {
         function App() {
-            this.message = 'Hello World!';
         }
+        App.prototype.configureRouter = function (config, router) {
+            this.router = router;
+            config.title = 'Isomorfismos';
+            config.map([
+                { route: '', redirect: 'searchword' },
+                { route: 'searchword', name: 'searchword', moduleId: 'searchword', nav: true, title: 'Busrcar palabra' }
+            ]);
+            config.mapUnknownRoutes('not-found');
+        };
         return App;
     }());
     exports.App = App;
@@ -40,6 +48,16 @@ define('main',["require", "exports", "./environment"], function (require, export
     exports.configure = configure;
 });
 
+define('searchword',["require", "exports"], function (require, exports) {
+    "use strict";
+    var SearchWord = (function () {
+        function SearchWord() {
+        }
+        return SearchWord;
+    }());
+    exports.SearchWord = SearchWord;
+});
+
 define('resources/index',["require", "exports"], function (require, exports) {
     "use strict";
     function configure(config) {
@@ -47,6 +65,7 @@ define('resources/index',["require", "exports"], function (require, exports) {
     exports.configure = configure;
 });
 
-define('text!app.html', ['module'], function(module) { module.exports = "<template>\n  <h1>${message}</h1>\n</template>\n"; });
+define('text!app.html', ['module'], function(module) { module.exports = "<template>\n  <require from=\"app.css\"></require>\n\n  <nav class=\"navbar navbar-default navbar-fixed-top\" role=\"navigation\">\n    <div class=\"navbar-header\">\n      <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\"\n              data-target=\"#skeleton-navigation-navbar-collapse\">\n        <span class=\"sr-only\">Toggle Navigation</span>\n        <span class=\"icon-bar\"></span>\n        <span class=\"icon-bar\"></span>\n        <span class=\"icon-bar\"></span>\n      </button>\n      <a class=\"navbar-brand\" href=\"#\">\n        <i class=\"fa fa-home\"></i>\n        <span>${router.title}</span>\n      </a>\n    </div>\n\n    <div class=\"collapse navbar-collapse\"\n         id=\"skeleton-navigation-navbar-collapse\">\n      <ul class=\"nav navbar-nav\">\n        <li repeat.for=\"row of router.navigation\"\n            class=\"${row.isActive ? 'active' : ''}\">\n          <a data-toggle=\"collapse\"\n             data-target=\"#skeleton-navigation-navbar-collapse.in\"\n             href.bind=\"row.href\">${row.title}</a>\n        </li>\n      </ul>\n\n      <ul class=\"nav navbar-nav navbar-right\">\n        <li class=\"loader\" if.bind=\"router.isNavigating\">\n          <i class=\"fa fa-spinner fa-spin fa-2x\"></i>\n        </li>\n      </ul>\n    </div>\n  </nav>\n\n  <div class=\"page-host\">\n    <router-view></router-view>\n  </div>\n</template>\n"; });
 define('text!app.css', ['module'], function(module) { module.exports = ".page-host {\r\n  position: absolute;\r\n  left: 0;\r\n  right: 0;\r\n  top: 50px;\r\n  bottom: 0;\r\n  overflow-x: hidden;\r\n  overflow-y: auto;\r\n}\r\n"; });
+define('text!searchword.html', ['module'], function(module) { module.exports = "<template>\r\n  <h1>Buscar palabra</h1>\r\n</template>\r\n"; });
 //# sourceMappingURL=app-bundle.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+
+import {Router, RouterConfiguration} from 'aurelia-router';
+
 export class App {
-  message = 'Hello World!';
+ public router: Router;
+ public configureRouter(config: RouterConfiguration, router: Router) {
+    this.router = router;
+    config.title = 'Isomorfismos';
+    config.map([
+      { route: '', redirect: 'searchword' },
+      { route: 'searchword', name: 'searchword', moduleId: 'searchword', nav: true, title: 'Busrcar palabra' }
+    ]);
+    config.mapUnknownRoutes('not-found');
+  }
 }
