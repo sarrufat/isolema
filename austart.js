b95357102812707fb02594e5f4a5089e37db3009
Sergi Arrufat <arrufat.sergi@gmail.com>
2017-02-26 12:04:12 +0100

    Added pm2 startup script

---
+++ austart.js
@@ -0,0 +1,32 @@
+"use strict";
+const resolve = require('/usr/lib/node_modules/aurelia-cli/lib/resolve');
+
+process.title = 'aurelia';
+
+let originalBaseDir = process.cwd();
+
+resolve('aurelia-cli', {
+  basedir: originalBaseDir
+}, function(error, projectLocalCli) {
+  let cli;
+
+  if (error) {
+    cli = new (require('../lib/index').CLI);
+    cli.options.runningGlobally = true;
+  } else {
+    cli = new (require(projectLocalCli).CLI);
+    cli.options.runningLocally = true;
+  }
+
+  cli.options.originalBaseDir = originalBaseDir;
+
+  let userArgs = process.argv.slice(2);
+  let commandName = userArgs[0];
+  let commandArgs = userArgs.slice(1);
+
+  cli.run(commandName, commandArgs).catch((error) => {
+      console.log(error);
+      process.exit(1);
+  });
+});
+
